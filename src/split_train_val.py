e959888aafd41d8e6f79e8d716266f3d26f1dffb
Mateusz Buda <mateusz@antipattern.pl>
2016-06-09 22:16:42 +0200

    Random drivers train-validation split to have viewport variety

--- src/split_train_val.py
+++ src/split_train_val.py
@@ -1,7 +1,9 @@
 import pandas as pd
 from shutil import move
 import os
+from random import shuffle
 
+imgsPath = 'imgs/'
 
 def main():
 	driversList = readDriversList()
@@ -23,20 +25,25 @@ def readDriversList():
 	return pd.read_csv('driver_imgs_list.csv', delimiter=",")
 
 
-def splitDrivers(drivers, trainSize=0.8):
-	subjectList = drivers['subject'].drop_duplicates()
+def splitDrivers(drivers, trainSize=0.75):
+	subjectList = drivers['subject'].drop_duplicates().tolist()
+	print(subjectList)
+	print(len(subjectList))
 	trainCount = int(len(subjectList) * trainSize)
+	print(trainCount)
+	shuffle(subjectList)
 	trainDrivers = subjectList[:trainCount]
-	validateDrivers = subjectList[trainCount + 1:]
+	validateDrivers = subjectList[trainCount+1:]
+	print(validateDrivers)
 
-	return trainDrivers.values, validateDrivers.values
+	return trainDrivers, validateDrivers
 
 
 def moveFiles(drivers, setList, setType):
 	for line in drivers[drivers['subject'].isin(setList)].values:
-		if not os.path.exists('imgs/' + setType + '/' + line[1]):
-			os.makedirs('imgs/' + setType + '/' + line[1])
-		move('imgs/train/' + line[1] + '/' + line[2], 'imgs/' + setType + '/' + line[1] + '/' + line[2])
+		if not os.path.exists(imgsPath + setType + '/' + line[1]):
+			os.makedirs(imgsPath + setType + '/' + line[1])
+		move(imgsPath + 'train/' + line[1] + '/' + line[2], imgsPath + setType + '/' + line[1] + '/' + line[2])
 
 
 if __name__ == "__main__":
