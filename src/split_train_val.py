b0754391b43429d16cb8b04ccc806889e513fc54
Mateusz Buda <mateusz@antipattern.pl>
2016-06-13 22:28:06 +0200

    New train-validation split not based on drivers

--- src/split_train_val.py
+++ src/split_train_val.py
@@ -2,48 +2,30 @@ import pandas as pd
 from shutil import move
 import os
 from random import shuffle
+import  numpy as np
 
 imgsPath = 'imgs/'
 
 def main():
 	driversList = readDriversList()
-	trainSubjects, validateSubjects = splitDrivers(driversList)
 
-	moveFiles(driversList, validateSubjects, 'validation')
+	driversList = driversList.reindex(np.random.permutation(driversList.index))
 
-	valImages = driversList[driversList['subject'].isin(validateSubjects)][['img', 'classname']]
-	trainImages = driversList[driversList['subject'].isin(trainSubjects)][['img', 'classname']]
+	driversList = driversList[['img', 'classname']]
 
-	trainImages['classname'] = trainImages['classname'].str.replace("c", "").astype(int)
-	valImages['classname'] = valImages['classname'].str.replace("c", "").astype(int)
+	driversList['classname'] = driversList['classname'].str.replace("c", "").astype(int)
 
-	trainImages.to_csv('trainset.csv', index=False, header=False, sep=" ")
-	valImages.to_csv('validationset.csv', index=False, header=False, sep=" ")
+	msk = np.random.rand(len(driversList)) < 0.8
+
+	train = driversList[msk]
+	valid = driversList[~msk]
+
+	train.to_csv('trainset.csv', index=False, header=False, sep=" ")
+	valid.to_csv('validset.csv', index=False, header=False, sep=" ")
 
 
 def readDriversList():
 	return pd.read_csv('driver_imgs_list.csv', delimiter=",")
-
-
-def splitDrivers(drivers, trainSize=0.75):
-	subjectList = drivers['subject'].drop_duplicates().tolist()
-	print(subjectList)
-	print(len(subjectList))
-	trainCount = int(len(subjectList) * trainSize)
-	print(trainCount)
-	shuffle(subjectList)
-	trainDrivers = subjectList[:trainCount]
-	validateDrivers = subjectList[trainCount+1:]
-	print(validateDrivers)
-
-	return trainDrivers, validateDrivers
-
-
-def moveFiles(drivers, setList, setType):
-	for line in drivers[drivers['subject'].isin(setList)].values:
-		if not os.path.exists(imgsPath + setType + '/' + line[1]):
-			os.makedirs(imgsPath + setType + '/' + line[1])
-		move(imgsPath + 'train/' + line[1] + '/' + line[2], imgsPath + setType + '/' + line[1] + '/' + line[2])
 
 
 if __name__ == "__main__":
